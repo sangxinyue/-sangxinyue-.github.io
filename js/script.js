21ccc296ad23347cac418d87628b71c0d7db2d15
sangxinyue <189672332@qq.com>
2025-10-31 14:31:16 +0800

    Site updated: 2025-10-31 14:31:16

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ document.ready(
     () => {
         const pagebody = document.getElementsByTagName('body')[0]
 
-        const default_theme = 'light' // 'dark'
+        const default_theme = 'dark'
 
         function setTheme(status = 'light') {
             if (status === 'dark') {
